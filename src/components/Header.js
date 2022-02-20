a4e14a5b38b9edfe520033e41f3d7496d51fd10c
Thomas McFadden <tmac727@yahoo.com>
2022-02-20 18:51:11 -0500

    add classname to li items in nav

--- src/components/Header.js
+++ src/components/Header.js
@@ -10,10 +10,11 @@ function Header() {
                 <nav>
                     <h1>McPadden Roofing Inc</h1>
                     <ul>
-                        <li>Home </li>
-                        <li>Services <FontAwesomeIcon icon={faWrench} /></li>
-                        <li>About Us</li>
-                        <li>Contact Us</li>
+                        <li className='nav-items'>Home </li>
+                        <li className='nav-items'>Services <FontAwesomeIcon icon={faWrench} /></li>
+                        <li className='nav-icons'><FontAwesomeIcon icon={faWrench} /></li>
+                        <li className='nav-items'>About Us</li>
+                        <li className='nav-items'>Contact Us</li>
                     </ul>
                 </nav>
                 </div>
