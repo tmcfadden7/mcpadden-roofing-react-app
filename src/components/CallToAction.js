d5326943e58ee4c65fb5a30c8cfdd2f73069a98c
Thomas McFadden <tmac727@yahoo.com>
2022-02-21 19:07:46 -0500

    add css class to section, add display flex to container, add h2 for the form

--- src/components/CallToAction.js
+++ src/components/CallToAction.js
@@ -3,13 +3,14 @@ import React from 'react'
 export const CallToAction = () => {
   return (
     <>
-        <section>
+        <section className='contact-container'>
             <div className="container">
                 <div className="contact-btn">
                     <h2>Free Estimates</h2>
                     <button>Call Us</button>
                 </div>
                 <div className="email-input">
+                    <h2>Sign up for more info</h2>
                     <form>
                         <input type="email" name="email" id="email" />
                         <input type="submit" value="submit" />
