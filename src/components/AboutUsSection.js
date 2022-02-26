1cae51d06e98729f592ab8adb6417c74608d7b63
Thomas McFadden <tmac727@yahoo.com>
2022-02-26 16:32:51 -0500

    add section/div containers, add img and our strory text with individual surrounding divs

--- src/components/AboutUsSection.js
+++ src/components/AboutUsSection.js
@@ -1,7 +1,17 @@
 function AboutUsSection() {
     return (
         <>
-        
+            <section className="about-section-container">
+                <div className="container">
+                    <div>
+                        <img src="" alt="" />
+                    </div>
+                    <div>
+                        <h2>Our Story</h2>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti, culpa recusandae natus tenetur quidem harum vitae odit sequi, molestias ex consequuntur beatae suscipit doloremque, perspiciatis nesciunt iusto unde. Eaque, libero? ... <a href="#">Read More</a></p>
+                    </div>
+                </div>
+            </section>
         </>
     );
 }
