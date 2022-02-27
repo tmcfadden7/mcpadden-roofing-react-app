040c60f03c75a0862de2f774e6e1913b711d308f
Thomas McFadden <tmac727@yahoo.com>
2022-02-27 12:50:24 -0500

    add bootstrap label, input and button for sign up box

--- src/components/CallToAction.js
+++ src/components/CallToAction.js
@@ -13,8 +13,12 @@ export const CallToAction = () => {
                     <div className="col-12 col-md-6">
                         <h2 className='h2'>Sign up for more info</h2>
                         <form>
-                            <input type="email" name="email" id="email" />
-                            <input type="submit" value="submit" />
+                            <div class="mb-3">
+                                <label for="exampleInputEmail1" class="form-label">Email address</label>
+                                <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
+                                <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+                            </div>
+                            <button type="submit" class="btn btn-primary">Submit</button>
                         </form>
                     </div>
                 </div>
