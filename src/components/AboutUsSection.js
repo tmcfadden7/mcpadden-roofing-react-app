dfba31d4bc22c70b13e208818f93a7ec81c2012f
Thomas McFadden <tmac727@yahoo.com>
2022-02-27 13:00:31 -0500

    add bootstrap container and rows

--- src/components/AboutUsSection.js
+++ src/components/AboutUsSection.js
@@ -6,12 +6,14 @@ function AboutUsSection() {
         <>
             <section className="about-section-container">
                 <div className="container">
-                    <div>
-                        <img className='img-fluid' src={workerImg} alt="" />
-                    </div>
-                    <div>
-                        <h2 className='h2'>Our Story</h2>
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti, culpa recusandae natus tenetur quidem harum vitae odit sequi, molestias ex consequuntur beatae suscipit doloremque, perspiciatis nesciunt iusto unde. Eaque, libero? ... <a href="#read-more">Read More</a></p>
+                    <div className="row">
+                        <div className="col-6 col-md-4">
+                            <img className='img-fluid' src={workerImg} alt="Construction Workers" />
+                        </div>
+                        <div className="col-12 col-md-8">
+                            <h2 className='h2'>Our Story</h2>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti, culpa recusandae natus tenetur quidem harum vitae odit sequi, molestias ex consequuntur beatae suscipit doloremque, perspiciatis nesciunt iusto unde. Eaque, libero? ... <a href="#read-more">Read More</a></p>
+                        </div>
                     </div>
                 </div>
             </section>
