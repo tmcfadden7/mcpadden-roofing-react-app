e2c8a4306399f32b514861dbafd8fd28fd96d565
Thomas McFadden <tmac727@yahoo.com>
2022-03-02 13:37:16 -0500

    change footer function declaration to function expression

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,4 +1,7 @@
-function Footer() {
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faHome, faIdCard, faMailBulk, faWrench } from '@fortawesome/free-solid-svg-icons'
+
+const Footer = () => {
     return (
         <>
             <footer>
@@ -8,11 +11,22 @@ function Footer() {
                             <h1>McPadden Roofing Inc</h1>
                         </div>
                         <div className="col-12 col-md-6">
-                            <ul>
-                                <li><a href="#home">Home</a></li>
-                                <li><a href="#home">Services</a></li>
-                                <li><a href="#home">About Us</a></li>
-                                <li><a href="#home">Contact Us</a></li>
+                            <ul class="list-group">
+                                <li class="list-group-item d-flex justify-content-between align-items-center">
+                                <a href="#home">Home <FontAwesomeIcon icon={faHome} /></a>
+                                </li>
+                                <li class="list-group-item d-flex justify-content-between align-items-center">
+                                    <a href="#home">Home</a>
+                                    <span class="badge bg-primary rounded-pill">2</span>
+                                </li>
+                                <li class="list-group-item d-flex justify-content-between align-items-center">
+                                <a href="#home">Home</a>
+                                    <span class="badge bg-primary rounded-pill">1</span>
+                                </li>
+                                <li class="list-group-item d-flex justify-content-between align-items-center">
+                                <a href="#home">Home</a>
+                                    <span class="badge bg-primary rounded-pill">1</span>
+                                </li>
                             </ul>
                         </div>
                     </div>
