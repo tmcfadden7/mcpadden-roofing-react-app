6629a63c244e7e3974269e807eeeb22e81b216c4
Thomas McFadden <tmac727@yahoo.com>
2022-03-02 13:53:47 -0500

    used nav around ul/li, removed bootstrap list form classes, add specific css target for header vs footer nav

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,39 +1,44 @@
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faHome, faIdCard, faMailBulk, faWrench } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {
+	faHome,
+	faIdCard,
+	faMailBulk,
+	faWrench,
+} from '@fortawesome/free-solid-svg-icons';
 
 const Footer = () => {
-    return (
-        <>
-            <footer>
-                <div className="container">
-                    <div className="row">
-                        <div className="col-12 col-md-6">
-                            <h1>McPadden Roofing Inc</h1>
-                        </div>
-                        <div className="col-12 col-md-6">
-                            <ul class="list-group">
-                                <li class="list-group-item d-flex justify-content-between align-items-center">
-                                <a href="#home">Home <FontAwesomeIcon icon={faHome} /></a>
-                                </li>
-                                <li class="list-group-item d-flex justify-content-between align-items-center">
-                                    <a href="#home">Home</a>
-                                    <span class="badge bg-primary rounded-pill">2</span>
-                                </li>
-                                <li class="list-group-item d-flex justify-content-between align-items-center">
-                                <a href="#home">Home</a>
-                                    <span class="badge bg-primary rounded-pill">1</span>
-                                </li>
-                                <li class="list-group-item d-flex justify-content-between align-items-center">
-                                <a href="#home">Home</a>
-                                    <span class="badge bg-primary rounded-pill">1</span>
-                                </li>
-                            </ul>
-                        </div>
-                    </div>
-                </div>
-            </footer>
-        </>
-    );
-}
+	return (
+		<>
+			<footer className='p-4'>
+				<div className='container'>
+					<nav>
+						<ul className='d-flex'>
+							<li>
+								<a href='#home'>
+									Home <FontAwesomeIcon icon={faHome} />
+								</a>
+							</li>
+							<li>
+								<a href='#home'>Home</a>
+							</li>
+							<li>
+								<a href='#home'>Home</a>
+							</li>
+							<li>
+								<a href='#home'>Home</a>
+							</li>
+						</ul>
+					</nav>
+					<div className='row'>
+						<div className='col-12 col-md-6'>
+							<h1>McPadden Roofing Inc</h1>
+						</div>
+						<div className='col-12 col-md-6'></div>
+					</div>
+				</div>
+			</footer>
+		</>
+	);
+};
 
 export default Footer;
