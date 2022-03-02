d377beae62f99c26f212c9af7e4bf34f8ba9ace9
Thomas McFadden <tmac727@yahoo.com>
2022-03-02 17:28:43 -0500

    add margin, padding classes

--- src/components/AboutUsSection.js
+++ src/components/AboutUsSection.js
@@ -1,24 +1,33 @@
 import workerImg from '../images/construction-workers.jpg';
 
 function AboutUsSection() {
-
-    return (
-        <>
-            <section className="about-section-container py-5">
-                <div className="container">
-                    <div className="row">
-                        <div className="col-6 col-md-4">
-                            <img className='img-fluid' src={workerImg} alt="Construction Workers" />
-                        </div>
-                        <div className="col-12 col-md-8">
-                            <h2 className='h2'>Our Story</h2>
-                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti, culpa recusandae natus tenetur quidem harum vitae odit sequi, molestias ex consequuntur beatae suscipit doloremque, perspiciatis nesciunt iusto unde. Eaque, libero? ... <a href="#read-more">Read More</a></p>
-                        </div>
-                    </div>
-                </div>
-            </section>
-        </>
-    );
+	return (
+		<>
+			<section className='about-section-container p-5'>
+				<div className='container'>
+					<div className='row'>
+						<div className='col-6 col-md-4'>
+							<img
+								className='img-fluid'
+								src={workerImg}
+								alt='Construction Workers'
+							/>
+						</div>
+						<div className='col-12 col-md-8'>
+							<h2 className='h2 mb-3'>Our Story</h2>
+							<p>
+								Lorem ipsum dolor sit amet consectetur adipisicing elit.
+								Deleniti, culpa recusandae natus tenetur quidem harum vitae odit
+								sequi, molestias ex consequuntur beatae suscipit doloremque,
+								perspiciatis nesciunt iusto unde. Eaque, libero? ...{' '}
+								<a href='#read-more'>Read More</a>
+							</p>
+						</div>
+					</div>
+				</div>
+			</section>
+		</>
+	);
 }
 
 export default AboutUsSection;
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,45 +1,65 @@
-
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faHome, faIdCard, faMailBulk, faWrench } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {
+	faHome,
+	faIdCard,
+	faMailBulk,
+	faWrench,
+} from '@fortawesome/free-solid-svg-icons';
 
 function Header() {
-    return (
-        <>
-            <header className='jumbotron'>
-                <div className="container">
-                <nav>
-                    <h1 className='h1'>McPadden Roofing Inc</h1>
-                    <ul>
-                        <li className='nav-item'>
-                            <a href="#home">Home <FontAwesomeIcon icon={faHome} /></a>
-                        </li>
-                        <li className='nav-icon'>
-                            <a href="#home"><FontAwesomeIcon icon={faHome} /></a>
-                        </li>
-                        <li className='nav-item'>
-                            <a href="#services">Services <FontAwesomeIcon icon={faWrench} /></a>
-                        </li>
-                        <li className='nav-icon'>
-                            <a href="#services"><FontAwesomeIcon icon={faWrench} /></a>
-                        </li>
-                        <li className='nav-item'>
-                            <a href="#about-us">About Us <FontAwesomeIcon icon={faIdCard} /></a>
-                        </li>
-                        <li className='nav-icon'>
-                            <a href="#about-us"><FontAwesomeIcon icon={faIdCard} /></a>
-                        </li>
-                        <li className='nav-item'>
-                            <a href="#contact-us">Contact Us <FontAwesomeIcon icon={faMailBulk} /></a>
-                        </li>
-                        <li className='nav-icon'>
-                            <a href="#contact-us"><FontAwesomeIcon icon={faMailBulk} /></a>
-                        </li>
-                    </ul>
-                </nav>
-                </div>
-            </header>
-        </>
-    )
+	return (
+		<>
+			<header className='jumbotron'>
+				<div className='container'>
+					<nav>
+						<h1 className='h1'>McPadden Roofing Inc</h1>
+						<ul>
+							<li className='nav-item'>
+								<a href='#home'>
+									Home <FontAwesomeIcon icon={faHome} />
+								</a>
+							</li>
+							<li className='nav-icon'>
+								<a href='#home'>
+									<FontAwesomeIcon icon={faHome} />
+								</a>
+							</li>
+							<li className='nav-item'>
+								<a href='#services'>
+									Services <FontAwesomeIcon icon={faWrench} />
+								</a>
+							</li>
+							<li className='nav-icon'>
+								<a href='#services'>
+									<FontAwesomeIcon icon={faWrench} />
+								</a>
+							</li>
+							<li className='nav-item'>
+								<a href='#about-us'>
+									About Us <FontAwesomeIcon icon={faIdCard} />
+								</a>
+							</li>
+							<li className='nav-icon'>
+								<a href='#about-us'>
+									<FontAwesomeIcon icon={faIdCard} />
+								</a>
+							</li>
+							<li className='nav-item'>
+								<a href='#contact-us'>
+									Contact Us <FontAwesomeIcon icon={faMailBulk} />
+								</a>
+							</li>
+							<li className='nav-icon'>
+								<a href='#contact-us'>
+									<FontAwesomeIcon icon={faMailBulk} />
+								</a>
+							</li>
+						</ul>
+					</nav>
+				</div>
+			</header>
+		</>
+	);
 }
 
 export default Header;
