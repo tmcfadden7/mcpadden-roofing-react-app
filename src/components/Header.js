59fb110fc73274ff2f05956481cb0f622b9695d4
Thomas McFadden <tmac727@yahoo.com>
2022-03-03 17:42:17 -0500

    rework entire nav styles, remove custom css, replace with bootstrap classes

--- src/components/Header.js
+++ src/components/Header.js
@@ -12,44 +12,55 @@ function Header() {
 			<header className='jumbotron'>
 				<div className='container'>
 					<nav>
-						<h1 className='h1'>McPadden Roofing Inc</h1>
-						<ul>
-							<li className='nav-item'>
+						<div className='d-flex justify-content-around align-items-center'>
+							<h1 className='h1 p-2'>McPadden Roofing Inc</h1>
+							<div>
+								<p>
+									<strong>Serving all of NY/FL</strong>
+								</p>
+								<p className='my-2'>
+									<strong>Call now! 555-555-5555</strong>
+								</p>
+							</div>
+						</div>
+
+						<ul className='d-flex justify-content-around align-items-center'>
+							<li className='nav-item d-none d-md-block p-3'>
 								<a href='#home'>
 									Home <FontAwesomeIcon icon={faHome} />
 								</a>
 							</li>
-							<li className='nav-icon'>
+							<li className='nav-icon h3 d-md-none'>
 								<a href='#home'>
 									<FontAwesomeIcon icon={faHome} />
 								</a>
 							</li>
-							<li className='nav-item'>
+							<li className='nav-item d-none d-md-block p-3'>
 								<a href='#services'>
 									Services <FontAwesomeIcon icon={faWrench} />
 								</a>
 							</li>
-							<li className='nav-icon'>
+							<li className='nav-icon h3 d-md-none'>
 								<a href='#services'>
 									<FontAwesomeIcon icon={faWrench} />
 								</a>
 							</li>
-							<li className='nav-item'>
+							<li className='nav-item d-none d-md-block p-3'>
 								<a href='#about-us'>
 									About Us <FontAwesomeIcon icon={faIdCard} />
 								</a>
 							</li>
-							<li className='nav-icon'>
+							<li className='nav-icon h3 d-md-none'>
 								<a href='#about-us'>
 									<FontAwesomeIcon icon={faIdCard} />
 								</a>
 							</li>
-							<li className='nav-item'>
+							<li className='nav-item d-none d-md-block p-3'>
 								<a href='#contact-us'>
 									Contact Us <FontAwesomeIcon icon={faMailBulk} />
 								</a>
 							</li>
-							<li className='nav-icon'>
+							<li className='nav-icon h3 d-md-none'>
 								<a href='#contact-us'>
 									<FontAwesomeIcon icon={faMailBulk} />
 								</a>
