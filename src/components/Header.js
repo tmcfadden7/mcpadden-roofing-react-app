2cfee7a06cf8cbc3c4bf480210b993dd61ed4871
Thomas McFadden <tmac727@yahoo.com>
2022-03-08 22:29:09 -0500

    add section for logo and adjust styles

--- src/components/Header.js
+++ src/components/Header.js
@@ -9,63 +9,68 @@ import {
 function Header() {
 	return (
 		<>
-			<header className='pt-3'>
-				<nav>
-					<div className='d-flex justify-content-around align-items-center'>
-						<h1 className='p-2 text-center'>McPadden Roofing</h1>
-						<div className='text-white'>
-							<p className='lead'>
-								<strong>Serving all of NY/FL</strong>
-							</p>
-							<p className='lead my-2'>
-								<strong>Call 555-555-5555</strong>
-							</p>
+			<header>
+				<section id='top-header' className='py-3'>
+					<div className='container'>
+						<div className='d-flex justify-content-around align-items-center'>
+							<h1 className='text-center'>McPadden Roofing</h1>
+							<div className='text-white'>
+								<p className='lead'>
+									<strong>Serving all of NY/FL</strong>
+								</p>
+								<p className='lead my-2'>
+									<strong>Call 555-555-5555</strong>
+								</p>
+							</div>
 						</div>
 					</div>
-
-					<ul className='d-flex justify-content-around align-items-center text-white'>
-						<li className='nav-item d-none d-md-block p-3'>
-							<a href='#home'>
-								Home <FontAwesomeIcon icon={faHome} />
-							</a>
-						</li>
-						<li className='nav-icon h3 d-md-none'>
-							<a href='#home'>
-								<FontAwesomeIcon icon={faHome} />
-							</a>
-						</li>
-						<li className='nav-item d-none d-md-block p-3'>
-							<a href='#services'>
-								Services <FontAwesomeIcon icon={faWrench} />
-							</a>
-						</li>
-						<li className='nav-icon h3 d-md-none'>
-							<a href='#services'>
-								<FontAwesomeIcon icon={faWrench} />
-							</a>
-						</li>
-						<li className='nav-item d-none d-md-block p-3'>
-							<a href='#about-us'>
-								About Us <FontAwesomeIcon icon={faIdCard} />
-							</a>
-						</li>
-						<li className='nav-icon h3 d-md-none'>
-							<a href='#about-us'>
-								<FontAwesomeIcon icon={faIdCard} />
-							</a>
-						</li>
-						<li className='nav-item d-none d-md-block p-3'>
-							<a href='#contact-us'>
-								Contact Us <FontAwesomeIcon icon={faMailBulk} />
-							</a>
-						</li>
-						<li className='nav-icon h3 d-md-none'>
-							<a href='#contact-us'>
-								<FontAwesomeIcon icon={faMailBulk} />
-							</a>
-						</li>
-					</ul>
-				</nav>
+				</section>
+				<div className='container'>
+					<nav>
+						<ul className='d-flex justify-content-around align-items-center text-white'>
+							<li className='nav-item d-none d-md-block p-3'>
+								<a href='#home'>
+									Home <FontAwesomeIcon icon={faHome} />
+								</a>
+							</li>
+							<li className='nav-icon h3 d-md-none'>
+								<a href='#home'>
+									<FontAwesomeIcon icon={faHome} />
+								</a>
+							</li>
+							<li className='nav-item d-none d-md-block p-3'>
+								<a href='#services'>
+									Services <FontAwesomeIcon icon={faWrench} />
+								</a>
+							</li>
+							<li className='nav-icon h3 d-md-none'>
+								<a href='#services'>
+									<FontAwesomeIcon icon={faWrench} />
+								</a>
+							</li>
+							<li className='nav-item d-none d-md-block p-3'>
+								<a href='#about-us'>
+									About Us <FontAwesomeIcon icon={faIdCard} />
+								</a>
+							</li>
+							<li className='nav-icon h3 d-md-none'>
+								<a href='#about-us'>
+									<FontAwesomeIcon icon={faIdCard} />
+								</a>
+							</li>
+							<li className='nav-item d-none d-md-block p-3'>
+								<a href='#contact-us'>
+									Contact Us <FontAwesomeIcon icon={faMailBulk} />
+								</a>
+							</li>
+							<li className='nav-icon h3 d-md-none'>
+								<a href='#contact-us'>
+									<FontAwesomeIcon icon={faMailBulk} />
+								</a>
+							</li>
+						</ul>
+					</nav>
+				</div>
 			</header>
 			<section className='jumbotron'>
 				<div className='container'></div>
